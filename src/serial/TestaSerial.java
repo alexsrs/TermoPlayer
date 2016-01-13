3c976f7f0ac6d038406330d0d2119696f5b8f7d0
alexsrs <alex@creapix.com.br>
2016-01-13 09:51:48 -0200

    Implementada as classes de comunicação serial
    Utilizando a biblioteca RXTX
    
    http://rxtx.qbang.org/wiki/index.php/Download

---
+++ src/serial/TestaSerial.java
@@ -0,0 +1,37 @@
+package serial;
+
+import serial.SerialCom;
+import serial.SerialComLeitura;
+
+public class TestaSerial extends SerialCom {
+
+	public static void main(String[] args) {
+		//Iniciando leitura serial
+
+        SerialComLeitura leitura = new SerialComLeitura("/dev/cu.usbmodemFD131", 9600, 0);
+
+        leitura.HabilitarLeitura();
+
+        leitura.ObterIdDaPorta();
+
+        leitura.AbrirPorta();
+
+        leitura.LerDados();
+
+        //Controle de tempo da leitura aberta na serial
+
+        try {
+        	
+            Thread.sleep(1000);
+
+        } catch (InterruptedException ex) {
+
+            System.out.println("Erro na Thread: " + ex);
+
+        }
+
+        leitura.FecharCom();
+
+    }
+
+}
